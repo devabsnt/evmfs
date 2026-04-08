87d1ecd53b01ab2c6b80c59cea7f664438420144
skibb <devskibb@gmail.com>
2026-04-08 14:58:01 +0700

    rainbowkit tweak

--- web/src/lib/wagmi.ts
+++ web/src/lib/wagmi.ts
@@ -1,11 +1,29 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import { metaMaskWallet, rabbyWallet, phantomWallet } from "@rainbow-me/rainbowkit/wallets";
+import { createConfig, http } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
 import { type Hex } from "viem";
 
-export const config = getDefaultConfig({
-  appName: "EVMFS",
-  projectId: "YOUR_WALLETCONNECT_PROJECT_ID",
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Wallets",
+      wallets: [metaMaskWallet, rabbyWallet, phantomWallet],
+    },
+  ],
+  {
+    appName: "EVMFS",
+    projectId: "YOUR_WALLETCONNECT_PROJECT_ID",
+  }
+);
+
+export const config = createConfig({
+  connectors,
   chains: [mainnet, sepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
 });
 
 export const EVMFS_CONTRACT: Hex = "0x140cbDFf649929D003091a5B8B3be34588753aBA";
