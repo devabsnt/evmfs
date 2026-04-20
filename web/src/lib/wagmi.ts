666da3860141455efaad244df048d0f478579126
skibb <devskibb@gmail.com>
2026-04-20 22:53:32 +0700

    update RPC provider

--- web/src/lib/wagmi.ts
+++ web/src/lib/wagmi.ts
@@ -22,7 +22,7 @@ export const EVMFS_CONTRACT: Hex = "0x140cbDFf649929D003091a5B8B3be34588753aBA";
 export const SUPPORTED_CHAIN_IDS = [1, 11155111] as const;
 
 export const DEFAULT_RPC_URLS: Record<number, string> = {
-  1: "https://eth.llamarpc.com",
+  1: "https://ethereum-rpc.publicnode.com",
   11155111: "https://ethereum-sepolia-rpc.publicnode.com",
 };
 
